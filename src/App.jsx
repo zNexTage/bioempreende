8d55f3c68d70d72b3c91c7bd93526c8761bcbc2b
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-11-14 19:39:28 -0300

    change file extension to jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Container from 'react-bootstrap/Container';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container fluid>
+      </Container>
+    </>
+  );
+}
+
+export default App;
