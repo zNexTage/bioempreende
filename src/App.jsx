5a54dcb6cebc56f65c4a5c990499dcd0e62843e6
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-11-22 20:31:57 -0300

    redid the layout of the initial section
    add bioempreende image;
    move the initial section to header component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import Header from "./components/header";
-import Container from 'react-bootstrap/Container';
-import Lottie from "lottie-react";
-import DnaBio from "./assets/lottiefiles/research.json";
-import { Button, Col, Row } from "react-bootstrap";
-import { WHATSAPP_URL } from "./utils/consts";
 import AboutUs from "./components/sections/about-us";
 import CountsInfo from "./components/sections/counts-info";
 import OurSolutions from "./components/sections/our-solutions";
@@ -12,50 +7,13 @@ import Footer from "./components/footer";
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
-      <section className="py-5 px-sm-2 px-md-3 px-lg-5">
-        <Container fluid>
-          <Row>
-            <Col md={12} xl={6} className="align-self-center">
-              <h1>
-                BioEmpreende
-              </h1>
-              <p class="lead text-justify">
-                Somos uma consultoria estratégica que nasceu para fazer a sua empresa prosperar. Entregamos os mais avançados conhecimentos sobre vendas e desenvolvimento de negócios no mercado 4.0 de forma objetiva e aplicável.
-              </p>
-              <Button as="a"
-                href={WHATSAPP_URL}
-                size="lg"
-                target="__blank"
-                style={{
-                  backgroundColor: "var(--bio-yellow)",
-                  border: "var(--bio-yellow)"
-                }}>
-                Fale conosco
-              </Button>
-            </Col>
-            <Col md={12} xl={6}>
-              <Lottie
-                animationData={DnaBio}
-                loop={true}
-                style={{
-                  height: 500
-                }}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-
       <CountsInfo />
-
       <AboutUs />
-
       <OurSolutions />
-
       <Footer />
-    </>
+    </main>
   );
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,25 +1,72 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import BioEmpreendeLogo from "../../assets/images/bioempreende-logo.png";
+import BioEmpreendeLogoIcon from "../../assets/icons/bioempreende-logo-icon.png";
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { WHATSAPP_URL } from '../../utils/consts';
+import Lottie from "lottie-react";
+import DnaBio from "../../assets/lottiefiles/dna-bio.json";
 
 
 
 const Header = () => {
     return (
         <header>
-            <Navbar bg="bio-dark-blue" expand="md" variant="dark" className="p-2" collapseOnSelect >
-                <Navbar.Brand href="#home">
-                    BioEmpreende
-                </Navbar.Brand>
+            <Navbar
+                bg="bio-dark-blue"
+                expand="md"
+                variant="dark"
+                className="p-2"
+                collapseOnSelect >
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
+                <Navbar.Collapse className='justify-content-end' id="responsive-navbar-nav">
+                    <Nav>
                         <Nav.Link href="#about-us">Sobre nós</Nav.Link>
                         <Nav.Link href="#services">Serviços</Nav.Link>
                         <Nav.Link href="#contact">Contato</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
+
+            <section className="py-5 px-sm-2 px-md-3 px-lg-5 bg-bio-dark-blue">
+                <Container fluid>
+                    <Row>
+                        <Col sm={12} md={6} xl={6} className="align-self-center">
+                            <img
+                                alt="Logo da BioEmpreende"
+                                src={BioEmpreendeLogo}
+                                className="w-100 mb-0 mb-md-5 mb-lg-5" />
+
+                            <p
+                                class="lead text-justify text-white mb-5">
+                                Somos uma consultoria estratégica que nasceu para fazer a sua empresa prosperar. Entregamos os mais avançados conhecimentos sobre vendas e desenvolvimento de negócios no mercado 4.0 de forma objetiva e aplicável.
+                            </p>
+                            <Button
+                                as="a"
+                                href={WHATSAPP_URL}
+                                size="lg"
+                                target="__blank"
+                                style={{
+                                    backgroundColor: "var(--bio-yellow)",
+                                    border: "var(--bio-yellow)"
+                                }}>
+                                Fale conosco
+                            </Button>
+                        </Col>
+                        <Col sm={12} md={6} xl={6}>
+                            <Lottie
+                                animationData={DnaBio}
+                                loop={true}
+                                style={{
+                                    height: 500
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
         </header>
     )
 }
